62b3fcce7c7357712a3ec80211d35e6e8f4f117a
karth <karth@192.168.1.65>
2023-06-25 16:09:14 +0530

    karthik

---
+++ VVIET/basics/for_loop.py
@@ -0,0 +1,40 @@
+#for x in range(1,6):
+#   print(x)
+
+#for x in range(0,6,2):
+#   print(x)
+
+#for x in range(0,6):
+#   print(x,end=" ")
+
+#for x in range(1,6,2):
+#   print(x,end=" ")
+'''
+*
+**
+***
+****
+*****
+'''
+#for pyramid
+#for x in range(1,6):
+#  print("*" *x)
+'''
+*****
+****
+***
+**
+*
+'''
+#for reverse pyramid
+#for x in range(6,0,-1):
+#    print("*" *x)
+
+'''
+start= +ve and -ve
+stop= +ve generally,-ve only if start point is lesser than stop point
+'''
+for x in range(10,0,-2):
+    print(x)
+
+print("execution completed")
