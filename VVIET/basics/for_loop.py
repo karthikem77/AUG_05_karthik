8f8dec620f31c97a6ea3d658845a07f25a58d13d
karth <karth@192.168.1.65>
2023-06-25 16:13:29 +0530

    virat

--- VVIET/basics/for_loop.py
+++ VVIET/basics/for_loop.py
@@ -33,8 +33,15 @@
 '''
 start= +ve and -ve
 stop= +ve generally,-ve only if start point is lesser than stop point
+step= +ve when start is lesser than stop
+      -ve when start is greater than stop
 '''
 for x in range(10,0,-2):
     print(x)
 
 print("execution completed")
+'''
+factorial and fibnocci series using control statements
+
+factorial- 4!=4*3*2*1=24
+fibnocci series- 0 1 1 2 3 5 8 13........
